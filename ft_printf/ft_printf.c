5aa106c67e4b85ec85880b9f995cd090f40968e7
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2024-06-12 12:52:22 +0200

    chore: Remove fr_printf directory and associated files

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,29 +6,23 @@
 /*   By: sfarren <sfarren@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 19:17:50 by sfarren           #+#    #+#             */
-/*   Updated: 2024/06/04 18:44:04 by sfarren          ###   ########.fr       */
+/*   Updated: 2024/06/07 21:07:37 by sfarren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+#include "./libft/libft.h"
 /*
-** ft_printf - print formatted output
-** @str: format string
-** @...: arguments
-**
-** Return: number of characters printed
-- Count number of arguments
-- Initialize the va_list
-- Iterate through the format string
-- Print characters until a '%' is found
-- increment counter for each character printed
-- Get the next character after the '%
-- Check if it is a valid conversion specifier
-- If it is, print the corresponding argument
-- If it is not, print the '%' and the character
-- Return the number of characters printed
-External functs. malloc, free, write,
-va_start, va_arg, va_copy, va_end
+
+Initialize a counter to keep track of the number of characters printed.
+Start iterating through the format string.
+If the current character is not '%', print it and increment the counter.
+If the current character is '%', check the next character.
+If the next character is a valid format specifier, use the corresponding function to print the argument and increment the counter by the number of characters printed.
+If the next character is not a valid format specifier, print the '%' and the character, and increment the counter by 2.
+Continue until the end of the format string.
+Return the counter
+
 */
 /*
 •%c Prints a single character.
@@ -41,29 +35,50 @@ va_start, va_arg, va_copy, va_end
 •%X Prints a number in hexadecimal (base 16) uppercase format.
 •%% Prints a percent sign.
 */
-int	ft_printf(const char *s, ...)
+int	ft_printf(const char *format, ...)
 {
 	va_list	args;
 	int		i;
 	int		print_counter;
-	char	*str;
 
 	print_counter = 0;
-	va_start(args, str);
-	while (str[i])
+	va_start(args, format);
+	i = 0;
+	while (format[i])
 	{
-		if (str[i] != '%')
+		if (format[i] != '%')
 		{
+			write(1, &format[i], 1);
 			print_counter++;
-			//
+			i++;
 		}
-		i++;
+		else
+		{
+			i++;
+			if (format[i] == '%')  // Add this case to handle '%%'
+			{
+				char p = '%';
+				write(1, &p, 1);
+				print_counter++;
+				i++;
+			}
+			else if (format[i] == 'c')
+			{
+				char c = va_arg(args, int);
+				write(1, &c, 1);
+				print_counter++;
+				i++;
+			}
+			// Add more cases for other format specifiers
+		}
 	}
-	return (0);
+	va_end(args);
+	return (print_counter);
 }
 
 int	main(void)
 {
-	ft_printf("Hello, %s!\n", "world");
+	// ft_printf("Hello, %s!\n", "world");
+	ft_printf("My char: %c", 'Z');
 	return (0);
 }
