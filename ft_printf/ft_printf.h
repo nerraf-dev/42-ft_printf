e2efeced7918f5d4ea27d4e962e80ef73b6aff60
Simon Farren <sfarren@c3r10s1.42malaga.com>
2024-07-17 13:11:08 +0200

    copy of campus work

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: sfarren <sfarren@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 19:20:31 by sfarren           #+#    #+#             */
-/*   Updated: 2024/07/17 12:52:32 by sfarren          ###   ########.fr       */
+/*   Updated: 2024/07/17 13:00:03 by sfarren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,70 @@
 #  define NULL_PTR_STR "(nil)"
 # endif
 
+/**
+ * Prints formatted output to the standard output stream.
+ *
+ * This function takes a format string `str` and a variable number of arguments,
+ * and prints the formatted output to the standard output stream. It supports
+ * various format specifiers, such as `%d` for integers, `%s` for strings, etc.
+ *
+ * @param str The format string specifying the output format.
+ * @param ... The variable number of arguments to be formatted and printed.
+ * @return total number of chars printed, or a negative value if error occurs.
+ */
 int		ft_printf(const char *str, ...);
+
+/**
+ * Prints a long long integer in a specified base.
+ *
+ * This function prints the given long long integer `n` in the specified `base`.
+ *  
+ * @param n     The long long integer to be printed.
+ * @param base  The string containing the characters used for the base.
+ * @param count A pointer to integer that tracks the num of characters printed.
+ */
 void	ft_printnbr_base(long long n, char *base, int *count);
+/**
+ * Prints a number in a specified base.
+ *
+ * @param n     The number to be printed.
+ * @param base  The base in which the number should be printed.
+ * @param count  keeps track of the number of characters printed.
+ */
+void	ft_printnbr_base(long long n, char *base, int *count);
+
+/**
+ * Converts an unsigned integer to a string representation.
+ *
+ * @param n The unsigned integer to be converted.
+ * @return  The string representation of the unsigned integer.
+ */
 char	*ft_utoa(unsigned int n);
+
+/**
+ * Prints a character.
+ *
+ * @param c     The character to be printed.
+ * @param count  keeps track of the number of characters printed.
+ */
 void	ft_printchr(char c, int *count);
+
+/**
+ * Prints a string.
+ *
+ * @param s     The string to be printed.
+ * @param count keeps track of the number of characters printed.
+ */
 void	ft_printstr(char *s, int *count);
+
+/**
+ * Prints a pointer address.
+ *
+ * @param ptr   The pointer address to be printed.
+ * @param count keeps track of the number of characters printed.
+ */
 void	ft_printptr(void *ptr, int *count);
-// int		ft_printhex(int n, char c);
-// int		ft_printu(unsigned int n);
-// int		parse_fstring(const char *str, va_list args);
-// int		get_next(const char *str, int i);
+
 // libft
 char	*ft_strchr(const char *s, int c);
 
